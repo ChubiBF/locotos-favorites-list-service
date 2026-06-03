2d7cd9b28f2220317bd2ebef92b213633081879b
Andres Baldiviezo <andres123bf@gmail.com>
2026-06-03 15:53:08 -0400

    cabecera autorization

--- app/main.py
+++ app/main.py
@@ -12,7 +12,8 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["Authorization", "Content-Type", "Accept"], 
+    expose_headers=["*"]
 )
 
 app.include_router(favorites_router)
